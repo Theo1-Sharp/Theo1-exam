d65c6b6c005a7a9e812a74296ce81d61c7bda054
Theo-T1 <141464997+Theo1-Sharp@users.noreply.github.com>
2023-08-12 15:32:22 +0300

    Change UI tests URL to Render

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,9 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+    await page.goto('theo1-exam.onrender.com/');
     const heading = await page.$('h1');
     const text = await heading.textContent();
     expect(text).toContain('Boardgames Collection');  
   });
-  
