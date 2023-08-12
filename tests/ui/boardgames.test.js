15d61d51baae8ff80e82d43e5365a7276901f6c0
Theo-T1 <141464997+Theo1-Sharp@users.noreply.github.com>
2023-08-12 16:01:50 +0300

    Added "https" to UI tests URL

--- tests/ui/boardgames.test.js
+++ tests/ui/boardgames.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check boardgames page', async ({ page }) => {
-    await page.goto('theo1-exam.onrender.com/boardgames');
+    await page.goto('https://theo1-exam.onrender.com/boardgames');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('theo1-exam.onrender.com/');
+    await page.goto('https://theo1-exam.onrender.com/');
     const heading = await page.$('h1');
     const text = await heading.textContent();
     expect(text).toContain('Boardgames Collection');  
